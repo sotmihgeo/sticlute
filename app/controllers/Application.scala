b1766f09153765611a83e1447f9531f0c60d02d5
Sotirca Mihaita George <sotirca.mihai@gmail.com>
2015-01-08 02:36:51 +0200

    lucru

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -10,7 +10,7 @@ object Application extends Controller {
 
   def index = Action {
     //ar trebui sa initializez userul global
-    Global.connectedUser = User("micky.sotirca@gmail.com", "", false, true)
+    Global.connectedUser = User("micky.sotirca@gmail.com", "", false, false)
     
     Ok(views.html.index("Colectie de sticlute mickyshor.ro", Global.connectedUser))
   }
